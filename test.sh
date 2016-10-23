25246979a126142dd4a84b557afb144e362ff2f1
DevNock <DevNock@gmail.com>
2016-10-24 00:31:14 +0300

    change * operation

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
+sudo sh load.sh
+
 op[0]="-"
 op[1]="+"
 op[2]="/"
-op[3]="*"
 for i in {0..2}
 do
 	first=$RANDOM
@@ -20,3 +21,21 @@ do
 		echo FALSE
 	fi	
 done
+
+first=$RANDOM
+second=$RANDOM
+let "cs = $first * $second"
+echo EXPECTED: $first "*" $second = $cs
+echo $first > /dev/first
+echo $second > /dev/second
+echo "*"  > /dev/operation
+res=$(cat /dev/result)
+echo GOT: $res
+if [[ "$res" -eq "$cs" ]]
+then 
+	echo TRUE
+else 
+	echo FALSE
+fi	
+
+sudo sh unload.sh
